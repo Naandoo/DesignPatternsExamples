7b741a63fe0d36c462c26d0eab017ecb24f4b470
Nando <Fernandocelso25@gmail.com>
2023-10-15 15:01:12 -0300

    [ADD] Final changes on Chain of Responsabilities application

--- DesignPatternExamples/Assets/Design Patterns/Chain Of Responsabilities/Word-Filter-Algorithmn/Scripts/SpellCheck.cs
+++ DesignPatternExamples/Assets/Design Patterns/Chain Of Responsabilities/Word-Filter-Algorithmn/Scripts/SpellCheck.cs
@@ -5,7 +5,7 @@ public class SpellCheck : MonoBehaviour
     [SerializeField] private ChatHandler _chatHandler;
     private IRuleHandler SpecialCharactersHandler = new SpecialCharactersHandlers();
     private IRuleHandler CharactersCountHandler = new CharactersCountHandler();
-    private string SpellCheckLog = " ";
+    private string SpellCheckLog = string.Empty;
 
     private void Awake() => SetOrderOfChain();
     private void SetOrderOfChain() => SpecialCharactersHandler.SetNext(CharactersCountHandler);
@@ -15,6 +15,10 @@ public class SpellCheck : MonoBehaviour
         AddLog(SpecialCharactersHandler.CheckRuleOnSentence(sentence));
 
         _chatHandler.AddChatBallon(SpellCheckLog, CharacterType.SecondCharacter);
+
+        CleanLog();
     }
+
+    private void CleanLog() => SpellCheckLog = string.Empty;
     public void AddLog(string Log) => SpellCheckLog += Log;
 }
