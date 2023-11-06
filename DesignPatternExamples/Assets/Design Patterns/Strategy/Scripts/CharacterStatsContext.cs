08385b41a3fe0dc0b919678026dcc1e8bd6d1355
Fernando Santos de Jesus <Fernandocelso25@gmail.com>
2023-11-06 18:05:20 -0300

    [ADD] Null check on Set Strategy method
    Co-authored-by: Ulisses Gandini <ugandini@gmail.com>

--- DesignPatternExamples/Assets/Design Patterns/Strategy/Scripts/CharacterStatsContext.cs
+++ DesignPatternExamples/Assets/Design Patterns/Strategy/Scripts/CharacterStatsContext.cs
@@ -1,6 +1,11 @@
 public class CharacterStatsContext
 {
     private IStrategy _strategy;
-    public void SetStrategy(IStrategy strategy) => _strategy = strategy;
+    public void SetStrategy(IStrategy strategy)
+    {
+        if (_strategy == null)
+            throw new NullReferenceException("Strategy cannot be null!");
+        _strategy = strategy;
+    }
     public CharacterStats GetCharacterStats() => _strategy.GetCharacterStats();
 }
