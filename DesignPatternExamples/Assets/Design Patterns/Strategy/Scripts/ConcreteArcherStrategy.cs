2d43e7454eb7e64ccee131041dbce27c5197dbd1
Fernando Santos de Jesus <Fernandocelso25@gmail.com>
2023-11-06 18:10:43 -0300

    [FIX] Applied changes on Strategy to improve algorithmn health
    Co-authored-by: Ulisses Gandini <ugandini@gmail.com>

--- DesignPatternExamples/Assets/Design Patterns/Strategy/Scripts/ConcreteArcherStrategy.cs
+++ DesignPatternExamples/Assets/Design Patterns/Strategy/Scripts/ConcreteArcherStrategy.cs
@@ -1,14 +1,14 @@
 public class ConcreteArcherStrategy : IStrategy
 {
-    public string _description => "A archer with the most deadly aim in the world, but only if he is drunk";
+    public string _description = "A archer with the most deadly aim in the world, but only if he is drunk";
 
-    public int _health => 45;
+    public int _health = 45;
 
-    public int _attackDamage => 50;
+    public int _attackDamage = 50;
 
-    public int _attackSpeed => 85;
+    public int _attackSpeed = 85;
 
-    public int _armor => 40;
+    public int _armor = 40;
 
     public CharacterStats GetCharacterStats() => new(_description, _health, _attackDamage, _attackSpeed, _armor);
 }
