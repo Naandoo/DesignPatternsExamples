55bd4427c9686b17e1458405b0ccb80accdd9b9d
Nando <Fernandocelso25@gmail.com>
2023-11-23 18:43:47 -0300

    [ADD] Added a little feedback to increase juiceness on sushi creation

--- DesignPatternExamples/Assets/Design Patterns/Abstract Factory/Assets/Scripts/IAbstractFactory.cs
+++ DesignPatternExamples/Assets/Design Patterns/Abstract Factory/Assets/Scripts/IAbstractFactory.cs
@@ -14,6 +14,7 @@ namespace AbstractFactory
         {
             GameObject sushi = PoolSystem.Get();
             sushi.transform.localPosition = _sushiCreationPoint.position;
+            AddRandomForce(sushi);
 
             ISushi iSushi = sushi.GetComponent<ISushi>();
 
@@ -21,5 +22,11 @@ namespace AbstractFactory
         }
 
         public void SetFactory(ISushi sushi) => sushi.Factory = this;
+
+        public void AddRandomForce(GameObject sushi)
+        {
+            Rigidbody rigidbody = sushi.GetComponentInChildren<Rigidbody>();
+            rigidbody.AddForce(new Vector3(Random.Range(-0.75f, 0.75f), 0f, Random.Range(-0.75f, 0.75f)) * 100f);
+        }
     }
 }
