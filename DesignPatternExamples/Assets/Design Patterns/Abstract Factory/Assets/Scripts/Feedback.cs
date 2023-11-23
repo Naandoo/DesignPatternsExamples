b6a70103b826df11256511a454d51562e54c20b4
Nando <Fernandocelso25@gmail.com>
2023-11-23 18:44:06 -0300

    [ADD] Improved the sushi feedback when eated

--- DesignPatternExamples/Assets/Design Patterns/Abstract Factory/Assets/Scripts/Feedback.cs
+++ DesignPatternExamples/Assets/Design Patterns/Abstract Factory/Assets/Scripts/Feedback.cs
@@ -8,9 +8,8 @@ namespace AbstractFactory
     {
         public void onEatAnimation(Transform transform, Action onFinishedAnimation)
         {
-            transform.DOLocalMoveY(1f, 0.5f).SetEase(Ease.OutQuad).OnComplete(() =>
+            transform.DOLocalMoveY(transform.position.y + 0.75f, 0.5f).SetEase(Ease.OutQuad).OnComplete(() =>
             {
-                transform.DOShakePosition(0.5f, 0.5f, 10, 90f, false, true);
                 transform.DOScale(0f, 0.5f).SetEase(Ease.InQuad).OnComplete(() =>
                 {
                     onFinishedAnimation?.Invoke();
